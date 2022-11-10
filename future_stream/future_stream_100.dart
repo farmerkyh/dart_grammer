08e547eef1b9b47055875e637cbbbaf7b957a1a7
farmerkyh <farmerkyh@naver.com>
2022-11-10 10:13:24 +0900

    .

--- future_stream/future_stream_100.dart
+++ future_stream/future_stream_100.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 /*
  async -> return Future<> : 일회성 응답에 사용
  async* -> return Stream<> : 여러번 가져올때 사용
@@ -12,36 +10,40 @@ import 'dart:async';
  
  참고 URL : https://beomseok95.tistory.com/308
 */
+
 Stream<int> countSream(int to) async* {
-	for(int i=1; i<=to; i++) {
-		print('countSream : $i');
-		yield i;
-	}
+  print('countSream start');
+  for (int i = 1; i <= to; i++) {
+    print('countSream : $i');
+    yield i;
+  }
 }
 
 Future<int> sumStream(Stream<int> stream) async {
-	var sum = 0;
-  print('sumStream start');  //한번만 실행 됨
-  
+  var sum = 0;
+  print('sumStream start'); //한번만 실행 됨
+
   //최종결과를 봐선 stream에 값이 넘겨져 올 경우 아래 문장을 수행하는 원리임
-	await for(var value in stream) {
-		print('value: $value');
-		sum += value;
-	}
-	return sum;
+  await for (var value in stream) {
+    print('value: $value');
+    sum += value;
+  }
+  return sum;
 }
 
-main() async{
-	var stream = countSream(5);
-  print('countSream end');  //한번만 실행 됨
-  //print('stream : $stream'); //결과 : stream : Instance of '_ControllerStream<int>'
-	var sum = await sumStream(stream);
-	print(sum);
+main() async {
+  //var stream = await countSream(5);
+  var stream = countSream(5); //위 문장과 왼쪽 문장은 동일하게 수행 됨
+  print('countSream next'); //한번만 실행 됨
+  var sum = await sumStream(stream);
+  print('sumStream end'); //한번만 실행 됨
+  print(sum);
 }
 
 /* 최종결과
-countSream end
-sumStream start
+countSream next    <-- countSream() 함수가 async이므로 먼저 수행 됨
+sumStream start    <-- ?  "countSream start" 가 먼저 수행 되어야 할거 같은데??? 왜???
+countSream start
 countSream : 1
 value: 1
 countSream : 2
@@ -52,6 +54,7 @@ countSream : 4
 value: 4
 countSream : 5
 value: 5
-15
- 
+sumStream end    <-- Stream과 await 에 의해서 countSream() <-> sumStream()함수가 반복적으로 수행됨
+                 <-- 신기 허네???
+15 
  */
